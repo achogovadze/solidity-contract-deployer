47ea60b86de840f05ed2899af9e286c24f57e883
Alexandre Chogovadze <alex.chogovadze@gmail.com>
2022-09-30 18:06:27 +0400

    added contract tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,6 +8,7 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const INITIAL_STRING = "Initial message";
 beforeEach(async () => {
   // get list of accounts
   accounts = await web3.eth.getAccounts();
@@ -15,13 +16,28 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Initial message"],
+      arguments: [INITIAL_STRING],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe("", () => {
-  it("", () => {
-    console.log(inbox);
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("matches the initial message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it("can change the message", async () => {
+    // set new message
+    const newMessage = "New message";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+
+    // get and check message
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
   });
 });
