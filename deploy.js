db386fc69f4a307537182a4c8e3251998e2ae239
Alexandre Chogovadze <alex.chogovadze@gmail.com>
2022-09-30 18:21:50 +0400

    setup infura provider

--- deploy.js
+++ deploy.js
@@ -1,1 +1,11 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const mnemonic =
+  "bundle jazz skate journey cool girl unaware wrap cushion absurd glare timber";
+const infuraUrl =
+  "https://goerli.infura.io/v3/ef829f345ace4a66bc90155a3d57650a";
+
+const provider = new HDWalletProvider(mnemonic, infuraUrl);
+const web3 = new Web3(provider);
